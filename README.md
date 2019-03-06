633c9df8b55a0bf1ec8cb35c12a11f18fdee4d84
Arran France <arran@arranfrance.com>
2019-03-06 17:03:34 +0000

    Initial docker version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Feeder
+
+A self-hosted Rust feed analytics platform designed along the lines of [Feedburner](https://feedburner.google.com) and [FeedPress](https://feed.press).
---
+++ api/src/main.rs
@@ -0,0 +1,22 @@
+
+extern crate actix_web;
+
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn greet(req: &HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", to)
+}
+
+fn main() {
+    println!("Binding to 8080");
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(greet))
+            .resource("/{name}", |r| r.f(greet))
+    })
+    .bind("0.0.0.0:8080")
+    .expect("Can not bind to port 8080")
+    .run();
+}
+
